c862f50961a2452404042d4e0e7697f867153d85
Kazuya Hatano <k28@me.com>
2022-12-27 21:35:27 +0900

    update

---
+++ section_02/code5_5_FibonacciNumber.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func fibo(_ n: Int) -> Int {
+    if n == 0 { return 0}
+    if n == 1 { return 1}
+
+    // 再帰呼び出し
+    return fibo(n - 1) + fibo(n - 2)
+}
+
+assert(fibo(0) == 0)
+assert(fibo(1) == 1)
+assert(fibo(2) == 1)
+assert(fibo(3) == 2)
+assert(fibo(4) == 3)
+assert(fibo(5) == 5)
+
+
