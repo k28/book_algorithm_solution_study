54521dcf189c142b0c3231edf9e875ad859acbf5
Kazuya Hatano <k28@me.com>
2023-01-04 07:29:02 +0900

    update

--- section_05/code_5_3_Frog.rb
+++ section_05/code_5_3_Frog.rb
@@ -11,7 +11,7 @@ h = [2, 9, 4, 5, 1, 6, 10]
 list = Array.new(N, INF)
 
 # listのi番目の物と b を比べてbの方が小さければ更新する
-def chmin(list, i, b)
+def choose_minimum(list, i, b)
   list[i] = b if list[i] > b
 end
 
@@ -20,11 +20,11 @@ list[0] = 0
 
 (1...N).each do |i|
   # 隣からの移動
-  chmin(list, i, (h[i - 1] - h[i]).abs + list[i - 1])
+  choose_minimum(list, i, (h[i - 1] - h[i]).abs + list[i - 1])
 
   # 1つ前からの移動
   if i - 2 >= 0
-    chmin(list, i, (h[i - 2] - h[i]).abs + list[i - 2])
+    choose_minimum(list, i, (h[i - 2] - h[i]).abs + list[i - 2])
   end
 end
 
--- section_05/code_5_3_Frog.swift
+++ section_05/code_5_3_Frog.swift
@@ -7,7 +7,7 @@ let N = 7
 let h = [2, 9, 4, 5, 1, 6, 10]
 var list: [UInt32] = Array(repeating: INF, count: N)
 
-func chmin<T:Comparable>(_ list: inout [T], _ i: Int, _ b: T) {
+func chooseMinimum<T:Comparable>(_ list: inout [T], _ i: Int, _ b: T) {
     if list[i] > b {
         list[i] = b
     }
@@ -18,11 +18,11 @@ list[0] = 0
 
 for i in 1..<N {
     // 隣からの移動
-    chmin(&list, i, UInt32(abs(h[i - 1] - h[i])) + list[i - 1])
+    chooseMinimum(&list, i, UInt32(abs(h[i - 1] - h[i])) + list[i - 1])
 
     // 1つ前からの移動
     if i - 2 >= 0 {
-        chmin(&list, i, UInt32(abs(h[i - 2] - h[i])) + list[i - 2])
+        chooseMinimum(&list, i, UInt32(abs(h[i - 2] - h[i])) + list[i - 2])
     }
 }
 
